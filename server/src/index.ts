2600d1419bedda86cbb4f335e135edb4898b8e0d
Maxell Milay <milaymaxell@gmail.com>
2022-09-11 15:01:23 +0800

    [Setup] initialize typescript in server

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello world update');
+});
+    
+const PORT = process.env.PORT || 5000;
+
+function start() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on PORT ${PORT} ....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
